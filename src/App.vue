2abf7fade9b1cea98472cbc63f4fadc618fa3f81
Jordan Wilia <jordan.k.wilia@gmail.com>
2022-12-09 13:43:59 -0800

    basic start to vuejs weight tracker app

---
+++ src/App.vue
@@ -0,0 +1,69 @@
+<script setup>
+  import { ref, shallowRef, computed, watch, nextTick } from 'vue'
+  import Chart from 'chart.js/auto'
+
+  const weights = ref([])
+
+  const weightCharEl = ref(null)
+
+  const weightChar = shallowRef(null)
+
+  const weightInput = ref(0)
+
+  const currentWeight = computed(() => {
+    return weights.value.sort((a,b) => b.date - a.date)[0] || { weight: 0 }
+  })
+
+  const addWeight = () => {
+    weights.value.push({
+      weight: weightInput.value,
+      date: new Date().getTime()
+    })
+  }
+
+  watch(weights, newWeights => {
+    const ws = [...newWeights]
+
+    console.log(ws)
+  }, { deep: true })
+</script>
+
+<template>
+  <main>
+    <h1>Weight Tracker</h1>
+    
+    <div class="current">
+      <span>{{currentWeight.weight}}</span>
+      <small>Current weight (lb)</small>
+    </div>
+
+    <form @submit.prevent="addWeight">
+      <input type="number" step="0.1" v-model="weightInput" >
+      <input type="submit" value="Add weight" >
+    </form>
+
+    <div v-if="weights && weights.length > 0">
+      <h2>Last 7 days</h2>
+
+      <div class="canvas-box">
+        <canvas ref="weightChartEl"></canvas>
+      </div>
+      
+      <div class="weight-history">
+        <h2>Weight History</h2>
+        <ul>
+          <li v-for="weight in weights">
+            <span>{{weight.weight}}lb</span>
+            <small>{{new Date(weight.date).toLocaleDateString()}}</small>
+          </li>
+        </ul>
+      </div>
+
+    </div>
+
+  </main>
+</template>
+
+<style>
+
+</style>
